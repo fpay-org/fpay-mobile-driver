86f06faabd3daa86e7b5f32e20cae5aac0023e90
Asiri Senith <asenith79@gmail.com>
2020-01-14 15:36:34 +0530

    Refactor code

---
+++ lib/fpay_driver_app.dart
@@ -0,0 +1,23 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fpay_driver/routes/application.dart';
+import 'package:fpay_driver/routes/routes.dart';
+
+class FPayDriverApp extends StatelessWidget {
+  FPayDriverApp() {
+    final router = Router();
+    Routes.configureRouter(router);
+    Application.router = router;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Officer App",
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: Application.router.generator,
+    );
+  }
+}
+  
