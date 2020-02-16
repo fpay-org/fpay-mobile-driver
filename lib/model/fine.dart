632b0e46c60503877f0393383c1628d31c64390e
Sashika Nawarathne <cazci269@gmail.com>
2020-02-16 18:30:56 +0530

    Implement models

---
+++ lib/model/fine.dart
@@ -0,0 +1,29 @@
+class Location {
+  final String longitude, latitude;
+
+  Location({this.longitude, this.latitude});
+}
+
+class Fine {
+  final String totalValue, currency, issuedAt;
+  final Location location;
+  final bool isPaid;
+
+  Fine(
+      {this.totalValue,
+      this.currency,
+      this.location,
+      this.issuedAt,
+      this.isPaid});
+
+  factory Fine.fromJson(Map<String, dynamic> json) {
+    return Fine(
+        totalValue: json['total_value'].toString(),
+        currency: json['currency'],
+        location: new Location(
+            longitude: json['location']['longitude'],
+            latitude: json['location']['latitude']),
+        issuedAt: json['issued_at'],
+        isPaid: json['is_paid']);
+  }
+}
